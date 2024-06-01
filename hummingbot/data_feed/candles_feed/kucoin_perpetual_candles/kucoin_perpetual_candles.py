d5beea2526287d4c0615470d79e27c1d8406aafa
drupman <tomasgaudino@hotmail.com>
2024-05-31 23:56:13 -0300

    (wip) add kucoin perpetual candles

--- hummingbot/data_feed/candles_feed/kucoin_perpetual_candles/kucoin_perpetual_candles.py
+++ hummingbot/data_feed/candles_feed/kucoin_perpetual_candles/kucoin_perpetual_candles.py
@@ -27,11 +27,23 @@ class KucoinPerpetualCandles(CandlesBase):
         return cls._logger
 
     def __init__(self, trading_pair: str, interval: str = "1min", max_records: int = 150):
+        self.symbols_dict = {}
         super().__init__(trading_pair, interval, max_records)
+        self.hb_base_asset = self._trading_pair.split("-")[0]
+        self.kucoin_base_asset = self.get_kucoin_base_asset()
+        self.quote_asset = self._trading_pair.split("-")[1]
+
+    def get_kucoin_base_asset(self):
+        for hb_asset, kucoin_value in CONSTANTS.HB_TO_KUCOIN_MAP.items():
+            return kucoin_value if hb_asset == self.hb_base_asset else self.hb_base_asset
 
     @property
     def name(self):
-        return f"kucoin_{self._trading_pair}"
+        return f"kucoin_perpetual_{self._trading_pair}"
+
+    @property
+    def interval_in_seconds(self):
+        return self.get_seconds_from_interval(self.interval)
 
     @property
     def rest_url(self):
@@ -39,7 +51,7 @@ class KucoinPerpetualCandles(CandlesBase):
 
     @property
     def wss_url(self):
-        return None
+        return CONSTANTS.WSS_URL
 
     @property
     def health_check_url(self):
@@ -72,42 +84,63 @@ class KucoinPerpetualCandles(CandlesBase):
                                              throttler_limit_id=CONSTANTS.HEALTH_CHECK_ENDPOINT)
         return NetworkStatus.CONNECTED
 
+    @property
+    def symbols_url(self):
+        return self.rest_url + CONSTANTS.SYMBOLS_ENDPOINT
+
+    async def generate_symbols_dict(self) -> Dict[str, Any]:
+        try:
+            rest_assistant = await self._api_factory.get_rest_assistant()
+            response = await rest_assistant.execute_request(url=self.symbols_url,
+                                                            throttler_limit_id=CONSTANTS.SYMBOLS_ENDPOINT)
+            symbols = response["data"]
+            symbols_dict = {}
+            for symbol in symbols:
+                symbols_dict[f"{symbol['baseCurrency']}-{symbol['quoteCurrency']}"] = symbol["symbol"]
+            self.symbols_dict = symbols_dict
+        except Exception:
+            self.logger().error("Error fetching symbols from Kucoin.")
+            raise
+
     def get_exchange_trading_pair(self, trading_pair):
-        return trading_pair
+        return f"{self.kucoin_base_asset}-{self.quote_asset}" if bool(self.symbols_dict) else None
+
+    async def symbols_ready(self):
+        while not bool(self.symbols_dict):
+            await self.generate_symbols_dict()
+            self._ex_trading_pair = self.get_exchange_trading_pair(self._trading_pair)
+        return bool(self._ex_trading_pair)
 
     async def fetch_candles(self,
                             start_time: Optional[int] = None,
                             end_time: Optional[int] = None,
-                            limit: Optional[int] = 1500):
+                            limit: Optional[int] = CONSTANTS.MAX_RECORDS_LIMIT):
         rest_assistant = await self._api_factory.get_rest_assistant()
-        params = {"symbol": self._ex_trading_pair, "type": CONSTANTS.INTERVALS[self.interval]}
+        params = {
+            "symbol": self.symbols_dict[f"{self.kucoin_base_asset}-{self.quote_asset}"],
+            "granularity": CONSTANTS.GRANULARITIES[self.interval],
+            "to": end_time if end_time else start_time + (limit * self.interval_in_seconds)
+        }
         if start_time:
-            params["startAt"] = start_time // 1000
-        if end_time:
-            params["endAt"] = end_time // 1000
-        else:
-            params["endAt"] = start_time // 1000 + (limit * self.get_seconds_from_interval(self.interval))
+            params["from"] = start_time
 
-        candles = await rest_assistant.execute_request(url=self.candles_url,
-                                                       throttler_limit_id=CONSTANTS.CANDLES_ENDPOINT,
-                                                       params=params)
-        candles = np.array([[row[0], row[1], row[3], row[4], row[2], row[5], row[6], 0., 0., 0.] for row in candles['data']]).astype(float)
-        candles[:, 0] = candles[:, 0] * 1000
-        return candles[::-1]
+        response = await rest_assistant.execute_request(url=self.candles_url,
+                                                        throttler_limit_id=CONSTANTS.CANDLES_ENDPOINT,
+                                                        params=params)
+        candles = np.array([[row[0], row[1], row[2], row[3], row[4], row[5], 0., 0., 0., 0.]
+                            for row in response['data']]).astype(float)
+        return candles
 
     async def fill_historical_candles(self):
-        max_request_needed = (self._candles.maxlen // 1500) + 1
+        max_request_needed = (self._candles.maxlen // CONSTANTS.MAX_RECORDS_LIMIT) + 1
         requests_executed = 0
+        await self.symbols_ready()
         while not self.ready:
-            # missing_records = self._candles.maxlen - len(self._candles)
+            missing_records = self._candles.maxlen - len(self._candles)
+            end_timestamp = int(self._candles[0][0])
             try:
                 if requests_executed < max_request_needed:
-                    end_timestamp = int(self._candles[-1][0] + 1000)
-                    # we have to add one more since, the last row is not going to be included
-                    start_time = (end_timestamp - (1500 * self.get_seconds_from_interval(self.interval)) * 1000) + 1000
-                    candles = await self.fetch_candles(end_time=end_timestamp, start_time=start_time, limit=1500)
-                    # we are computing agaefin the quantity of records again since the websocket process is able to
-                    # modify the deque and if we extend it, the new observations are going to be dropped.
+                    candles = await self.fetch_candles(end_time=end_timestamp, limit=missing_records + 1)
                     missing_records = self._candles.maxlen - len(self._candles)
                     self._candles.extendleft(candles[-(missing_records + 1):-1])
                     requests_executed += 1
@@ -129,12 +162,14 @@ class KucoinPerpetualCandles(CandlesBase):
         :param ws: the websocket assistant used to connect to the exchange
         """
         try:
+            await self.symbols_ready()
+            topic_candle = f"{self.symbols_dict[self._ex_trading_pair]}_{CONSTANTS.INTERVALS[self.interval]}"
             payload = {
                 "id": str(get_tracking_nonce()),
                 "type": "subscribe",
-                "topic": f"/market/candles:{self._ex_trading_pair}_{CONSTANTS.INTERVALS[self.interval]}",
+                "topic": f"/contractMarket/limitCandle:{topic_candle}",
                 "privateChannel": False,
-                "response": False,
+                "response": True,
             }
             subscribe_candles_request: WSJSONRequest = WSJSONRequest(payload=payload)
 
@@ -167,16 +202,18 @@ class KucoinPerpetualCandles(CandlesBase):
     async def _process_websocket_messages_from_candles(self, websocket_assistant: WSAssistant):
         async for ws_response in websocket_assistant.iter_messages():
             data: Dict[str, Any] = ws_response.data
-            if data is not None and data.get(
-                    "subject") == "trade.candles.update":  # data will be None when the websocket is disconnected
-                candles = data["data"]["candles"]
-                timestamp = int(candles[0]) * 1000
+            data = data.get("data", {})
+            # TODO: If there are no trades, socket doesn't update.
+            # TODO: This overrides fill_historical_candles, so it doesn't work properly.
+            if "candles" in data:
+                candles = data["candles"]
+                timestamp = int(candles[0]) * 1000.0
                 open = candles[1]
                 close = candles[2]
                 high = candles[3]
                 low = candles[4]
                 volume = candles[5]
-                quote_asset_volume = candles[6]
+                quote_asset_volume = 0.
                 n_trades = 0.
                 taker_buy_base_volume = 0.
                 taker_buy_quote_volume = 0.
@@ -186,7 +223,6 @@ class KucoinPerpetualCandles(CandlesBase):
                     self._candles.append(candles_array)
                     safe_ensure_future(self.fill_historical_candles())
                 elif timestamp > int(self._candles[-1][0]):
-                    # TODO: validate also that the diff of timestamp == interval (issue with 1M interval).
                     self._candles.append(candles_array)
                 elif timestamp == int(self._candles[-1][0]):
                     self._candles.pop()
